f9bf25aa104726d81246d20b2b4881d876eac090
FAIZAN AHMAD <faizanahmad8292@gmail.com>
2023-06-05 22:22:39 +0500

    ayat-range-section

--- app/src/main/java/com/example/quranapp/verses_activity.java
+++ app/src/main/java/com/example/quranapp/verses_activity.java
@@ -7,6 +7,8 @@ import android.media.Image;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +18,10 @@ public class verses_activity extends AppCompatActivity {
     QuranArabicText quranArabicText = new QuranArabicText();
     TextView surahName;
     TextView verses;
+
+    EditText startingNum,endingNum;
+
+    Button button;
 
     ImageView imageView;
 
@@ -37,20 +43,44 @@ public class verses_activity extends AppCompatActivity {
         surahName = findViewById(R.id.surahnametextview);
         surahName.setText(intent.getExtras().getString("surah"));
 
-        verses = findViewById(R.id.versesTextView);
+        startingNum = findViewById(R.id.startingNumber);
+        endingNum = findViewById(R.id.endingNumber);
+        button = findViewById(R.id.button);
 
         int index=intent.getExtras().getInt("index");
 
         int start=obj.SSP[index];
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int startn = Integer.parseInt(String.valueOf(startingNum.getText()));
+                int endn = Integer.parseInt(String.valueOf(endingNum.getText()));
+
+
+                verses.setMovementMethod(new ScrollingMovementMethod());
+                verses.setText(quranArabicText.QuranArabicText[0]+"\n");
+                loadVerses(start+startn-1,start+endn);
+            }
+        });
+
+        verses = findViewById(R.id.versesTextView);
+
+
         int end=start+obj.surahAyatCount[index];
 
         verses.setMovementMethod(new ScrollingMovementMethod());
         verses.setText(quranArabicText.QuranArabicText[0]+"\n");
 
+        loadVerses(start,end);
+    }
+
+    private void loadVerses(int start,int end)
+    {
         for (int i=start;i<end;i++)
         {
             verses.append("  "+quranArabicText.QuranArabicText[i]);
         }
+
     }
 
 }

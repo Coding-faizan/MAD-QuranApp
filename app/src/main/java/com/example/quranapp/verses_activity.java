856f459f23e6d1ff1b3ba4b24210fd1c69944b4d
FAIZAN AHMAD <faizanahmad8292@gmail.com>
2023-06-05 20:29:20 +0500

    vertical-scroll-added

--- app/src/main/java/com/example/quranapp/verses_activity.java
+++ app/src/main/java/com/example/quranapp/verses_activity.java
@@ -3,7 +3,9 @@ package com.example.quranapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,12 +17,22 @@ public class verses_activity extends AppCompatActivity {
     TextView surahName;
     TextView verses;
 
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verses);
 
         Intent intent = getIntent();
+
+        imageView = findViewById(R.id.backButton);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
         surahName = findViewById(R.id.surahnametextview);
         surahName.setText(intent.getExtras().getString("surah"));
@@ -32,7 +44,7 @@ public class verses_activity extends AppCompatActivity {
         int start=obj.SSP[index];
         int end=start+obj.surahAyatCount[index];
 
-
+        verses.setMovementMethod(new ScrollingMovementMethod());
         verses.setText(quranArabicText.QuranArabicText[0]+"\n");
 
         for (int i=start;i<end;i++)
